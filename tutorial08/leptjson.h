c27b16c072346e585f8de4898239ebacd79f91d4
oshino <B1747322151@outlook.com>
2024-04-08 01:58:24 +0800

    impl: 08

--- tutorial08/leptjson.h
+++ tutorial08/leptjson.h
@@ -3,7 +3,15 @@
 
 #include <stddef.h> /* size_t */
 
-typedef enum { LEPT_NULL, LEPT_FALSE, LEPT_TRUE, LEPT_NUMBER, LEPT_STRING, LEPT_ARRAY, LEPT_OBJECT } lept_type;
+typedef enum {
+  LEPT_NULL,
+  LEPT_FALSE,
+  LEPT_TRUE,
+  LEPT_NUMBER,
+  LEPT_STRING,
+  LEPT_ARRAY,
+  LEPT_OBJECT
+} lept_type;
 
 #define LEPT_KEY_NOT_EXIST ((size_t)-1)
 
@@ -11,38 +19,52 @@ typedef struct lept_value lept_value;
 typedef struct lept_member lept_member;
 
 struct lept_value {
-    union {
-        struct { lept_member* m; size_t size, capacity; }o; /* object: members, member count, capacity */
-        struct { lept_value*  e; size_t size, capacity; }a; /* array:  elements, element count, capacity */
-        struct { char* s; size_t len; }s;                   /* string: null-terminated string, string length */
-        double n;                                           /* number */
-    }u;
-    lept_type type;
+  union {
+    struct {
+      lept_member* m;
+      size_t size, capacity;
+    } o; /* object: members, member count, capacity */
+    struct {
+      lept_value* e;
+      size_t size;
+      size_t capacity;
+    } a;
+    struct {
+      char* s;
+      size_t len;
+    } s;      /* string: null-terminated string, string length */
+    double n; /* number */
+  } u;
+  lept_type type;
 };
 
 struct lept_member {
-    char* k; size_t klen;   /* member key string, key string length */
-    lept_value v;           /* member value */
+  char* k;
+  size_t klen;  /* member key string, key string length */
+  lept_value v; /* member value */
 };
 
 enum {
-    LEPT_PARSE_OK = 0,
-    LEPT_PARSE_EXPECT_VALUE,
-    LEPT_PARSE_INVALID_VALUE,
-    LEPT_PARSE_ROOT_NOT_SINGULAR,
-    LEPT_PARSE_NUMBER_TOO_BIG,
-    LEPT_PARSE_MISS_QUOTATION_MARK,
-    LEPT_PARSE_INVALID_STRING_ESCAPE,
-    LEPT_PARSE_INVALID_STRING_CHAR,
-    LEPT_PARSE_INVALID_UNICODE_HEX,
-    LEPT_PARSE_INVALID_UNICODE_SURROGATE,
-    LEPT_PARSE_MISS_COMMA_OR_SQUARE_BRACKET,
-    LEPT_PARSE_MISS_KEY,
-    LEPT_PARSE_MISS_COLON,
-    LEPT_PARSE_MISS_COMMA_OR_CURLY_BRACKET
+  LEPT_PARSE_OK = 0,
+  LEPT_PARSE_EXPECT_VALUE,
+  LEPT_PARSE_INVALID_VALUE,
+  LEPT_PARSE_ROOT_NOT_SINGULAR,
+  LEPT_PARSE_NUMBER_TOO_BIG,
+  LEPT_PARSE_MISS_QUOTATION_MARK,
+  LEPT_PARSE_INVALID_STRING_ESCAPE,
+  LEPT_PARSE_INVALID_STRING_CHAR,
+  LEPT_PARSE_INVALID_UNICODE_HEX,
+  LEPT_PARSE_INVALID_UNICODE_SURROGATE,
+  LEPT_PARSE_MISS_COMMA_OR_SQUARE_BRACKET,
+  LEPT_PARSE_MISS_KEY,
+  LEPT_PARSE_MISS_COLON,
+  LEPT_PARSE_MISS_COMMA_OR_CURLY_BRACKET
 };
 
-#define lept_init(v) do { (v)->type = LEPT_NULL; } while(0)
+#define lept_init(v)       \
+  do {                     \
+    (v)->type = LEPT_NULL; \
+  } while (0)
 
 int lept_parse(lept_value* v, const char* json);
 char* lept_stringify(const lept_value* v, size_t* length);
@@ -90,7 +112,7 @@ const char* lept_get_object_key(const lept_value* v, size_t index);
 size_t lept_get_object_key_length(const lept_value* v, size_t index);
 lept_value* lept_get_object_value(lept_value* v, size_t index);
 size_t lept_find_object_index(const lept_value* v, const char* key, size_t klen);
-lept_value* lept_find_object_value(lept_value* v, const char* key, size_t klen);
+lept_value* lept_find_object_value(const lept_value* v, const char* key, size_t klen);
 lept_value* lept_set_object_value(lept_value* v, const char* key, size_t klen);
 void lept_remove_object_value(lept_value* v, size_t index);
 
